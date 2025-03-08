88c8df5d147967c5ca16d96cf617ce54e37962ee
github-action-benchmark <github@users.noreply.github.com>
2025-03-08 10:39:19 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 29c4e8dcbf729c0496afc0bf3239d225cb78366e

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1741430359119,
+  "repoUrl": "https://github.com/Boyquotes/godot-solana-sdk",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "axel.benjaminsson@live.se",
+            "name": "Axel",
+            "username": "Virus-Axel"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "29c4e8dcbf729c0496afc0bf3239d225cb78366e",
+          "message": "Merge pull request #335 from Tanv33/master\n\n🚀 Honeycomb Integration: Methods, Resources, and Testing Enhancements",
+          "timestamp": "2025-03-08T08:17:18+01:00",
+          "tree_id": "73a70ad4cc96c8c93abcfb3794e2b87d09708f58",
+          "url": "https://github.com/Boyquotes/godot-solana-sdk/commit/29c4e8dcbf729c0496afc0bf3239d225cb78366e"
+        },
+        "date": 1741430357937,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Simple Transaction Performance Index",
+            "value": 0.356164383561644,
+            "unit": "Percent"
+          }
+        ]
+      }
+    ]
+  }
+}
